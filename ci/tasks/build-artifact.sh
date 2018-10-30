49f73b965611df0d08277c1aa03cb208032a8b34
Chavan <shruti.vilankar@gmail.com>
2018-10-30 15:36:57 -0400

    change artifacts

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-export GRADLE_OPTS=-Dorg.gradle.native=false
-version=`cat version/number`
-cd git-repo
-gradle assemble -Pversion=$version
-cp build/libs/*.jar ../artifact-dir/
+echo "hurray"
